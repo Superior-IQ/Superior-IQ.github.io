abd2b5feacf2c3ff3bf269c1f1084b7a1b4bc26e
amirhossein-github <amirhossein.business.developer@gmail.com>
2024-08-30 15:36:29 +0330

    refactor: Return to previous changes
    commitID: 35894584b8ecf46a68e3f907261e2f03dbf42aaf

--- script.js
+++ script.js
@@ -1,87 +1,54 @@
-// [ Initialize Variables ]
-
-let quizImagePath = './Images/quiz'
-/** This array stores all the addresses of the quiz images */
-let quizImages = []
-let quizListOfAllIds = []
-let quizStatus = 0
+import { comparTwoObjects, getRandomIndexOfArray } from './modules/utils.js';
+import { setQuizImages, setQuizTrueResults } from "./modules/methods.js";
 
 /**
- * This function indexing all quiz images in the array
- * @param {array} array An array to store all the quiz images
+ * This array stores all the addresses of the quiz images
  */
-function setQuizImages(array) {
-
-    // It covers all 30 folders: from 1 to 30
-    for (let i = 0; i < 30; i++) {
-        const testNumber = i + 1
-
-        array.push(
-            {
-                id: `quiz${testNumber}`,
-                imageQuestion: `${quizImagePath}/${testNumber}/test${testNumber}.png`,
-                imageAnswers: []
-            }
-        )
-
-        // from directory 1 - 12 that they are contain 6 images
-        if (i <= 11) {
-            for (let x = 1; x <= 6; x++) {
-                array[i].imageAnswers.push(
-                    `${quizImagePath}/${testNumber}/${testNumber}-${x}.png`
-                )
-            }
-
-            // from directory 13 - 30 that they are contain 8 images
-        } else {
-            for (let x = 1; x <= 8; x++) {
-                array[i].imageAnswers.push(
-                    `${quizImagePath}/${testNumber}/${testNumber}-${x}.png`
-                )
-            }
-        }
-    }
-
-}; setQuizImages(quizImages)
+let quizzes = setQuizImages()
 
 /**
- * This function collects all quizzes id in a list
- * @param {array} array An array to store all the IDs
+ * Save all the correct quiz answers based on the quiz ID
  */
-function setQuizListAllId(array) {
-    for (let id = 0; id < quizImages.length; id++) {
-        array.push(quizImages[id].id)
-    }
-}; setQuizListAllId(quizListOfAllIds)
+const quizAnswers = setQuizTrueResults()
 
 /**
- * This function randomly selects an object from the array and finally deletes it
- * @param {array} array An array of objects: [{}, {}, {}, ...]
- * @returns {number} Random number between 0 to array.length
+ * It stores all the information about the user
  */
-function getRandomQuiz(array) {
-
-    // A) Receive quiz randomly
-    let randomIndex = Math.floor(Math.random() * array.length)
-    let randomQuiz = array[randomIndex]
-
-    // B) get images path
-    let randomQuizPath = {}
-    for (let i = 0; i < quizImages.length; i++) {
-        if (quizImages[i].id === randomQuiz) {
-            randomQuizPath = quizImages[i]
-            break
-        }
-    }
-
-    // remove element
-    array.splice(randomIndex, 1)
-
-    return randomQuizPath
+let user = {
+    score: 0,
+    name: '',
+    age: 0,
+    quizResults: {},
+    cheat: false
 }
 
-/** This function implements the HTML structure of the welcome panel section */
-function injectTemplateWelcome() {
+/**
+ * The general task of this function is to control the process of performing quiz
+ * - When to go to the next quiz and when to show the results of the quizzes
+ */
+function quizHandler(){
+
+    // If all quizzes are completed
+    if (quizzes.length === 0){
+
+        // 1) Calculate user answer
+        user.score = comparTwoObjects(quizAnswers, user.quizResults).true
+
+        // 2) Show result
+        return injectTemplateShowResult()
+
+    } else {
+
+        // Go to the next quiz
+        injectTemplateQuiz(
+            getRandomIndexOfArray(quizzes, true)
+        )
+
+    }
+
+}
+
+function injectTemplateWelcome(){
     /*
     
     <div>
@@ -97,25 +64,27 @@ function injectTemplateWelcome() {
     let mainSpace = document.createElement('div')
     mainSpace.id = 'mainSpace'
     document.body.appendChild(mainSpace)
-
+    
     // <h1> name of project
     let appName = document.createElement('h1')
     appName.id = 'appName'
     appName.innerText = 'Superior IQ'
     mainSpace.appendChild(appName)
-
+    
     // <input> get name
     let userNameInput = document.createElement('input')
     userNameInput.className = 'textField'
     userNameInput.type = 'text'
     userNameInput.placeholder = 'Name'
+    userNameInput.id = 'userName'
     mainSpace.appendChild(userNameInput)
-
+    
     // <input> get age
     let userAgeInput = document.createElement('input')
     userAgeInput.className = 'textField'
     userAgeInput.type = 'number'
     userAgeInput.placeholder = 'Age'
+    userAgeInput.id = 'userAge'
     mainSpace.appendChild(userAgeInput)
 
     // <input> start btn
@@ -125,46 +94,44 @@ function injectTemplateWelcome() {
     startBtn.value = 'Start'
     mainSpace.appendChild(startBtn)
     // if start button pressed: 
-    startBtn.addEventListener("click", function () {
-        nextQuiz()
+    startBtn.addEventListener("click", function(){
+
+        // control user input: age, username, cheat
+        let isUserName = document.getElementById("userName").value !== ""
+        let isUserAge = (
+            document.getElementById("userAge").value !== "" &&
+            document.getElementById("userAge").value >= 6 &&
+            document.getElementById("userAge").value <= 50
+        )
+        //cheating: mange to active cheat
+        user.cheat = document.getElementById("userName").value === "cheat" && document.getElementById("userAge").value === '0'
+        isUserAge = user.cheat || isUserAge
+
+        if(isUserName && isUserAge){
+
+            // 1) Store values
+            user.name = document.getElementById("userName").value
+            user.age = document.getElementById("userAge").value
+
+            // 2) Conducting quizzes
+            quizHandler()
+        }
+
     });
 }
 
-/** The general task of this function is to control the process of performing quiz */
-function nextQuiz() {
-
-    // 0) change quiz status
-    if (quizStatus < quizImages.length) {
-        quizStatus++
-    } else {
-        return injectTemplateShowResult()
-    }
-
-    // 1) Empty all space
-    document.getElementById('mainSpace').innerHTML = ''
-
-    // 2) Get random image addresses of quiz
-    let quizPaths = getRandomQuiz(quizListOfAllIds)
-
-    // 3) Implement HTML code
-    console.log(quizPaths)
-    injectTemplateQuiz(quizPaths)
-
-}
-
-/**
- * This function implements the HTML part of the quiz
- * @param {object} path contain path of images
- */
-function injectTemplateQuiz(path) {
+function injectTemplateQuiz(path){
     /*
- 
-    <img>
-    <div>
-        <img> * (6 || 8)
-    </div>
+    
+        <img>
+        <div>
+            <img> * (6 || 8)
+        </div>
 
-*/
+    */
+
+    // Free up space 
+    document.getElementById('mainSpace').innerHTML = ''
 
     // <img> question image
     let questionImg = document.createElement('img')
@@ -176,26 +143,38 @@ function injectTemplateQuiz(path) {
     // <div> to put all answerImages
     let answerDiv = document.createElement('div')
     // <img> making tags of answer images one by one
-    for (let i = 0; i < path.imageAnswers.length; i++) {
+    let len = path.imageAnswers.length
+    let zeroTolLength = Array.from(Array(len).keys())
+    for (let i = 0; i < len; i++) {
+        let index = getRandomIndexOfArray(zeroTolLength, true)
         let answerImg = document.createElement('img')
         answerImg.setAttribute('width', '70px')
-        answerImg.setAttribute('src', path.imageAnswers[i])
-        answerImg.addEventListener("click", function () {
-            nextQuiz()
+        answerImg.setAttribute('src', path.imageAnswers[index])
+        answerImg.setAttribute('id', `option-${index+1}`)
+        // Cheat section: start
+        if (user.cheat){
+            if (quizAnswers[path.id] === answerImg.id){ // The option with the correct answer was one
+                answerImg.className = 'cheat-showResult'
+
+            }
+        }
+        // Cheat section: end
+        answerImg.addEventListener("click", function(){
+            // save user selection
+            Object.assign(user.quizResults, {[path.id]: this.id})
+            quizHandler()
         });
         answerDiv.appendChild(answerImg)
     }
     mainSpace.appendChild(answerDiv)
 }
 
-/**
- * Implementation of the final panel that is supposed to show the result to the user
- */
-function injectTemplateShowResult() {
+function injectTemplateShowResult(){
+    // Free up space 
+    document.getElementById('mainSpace').innerHTML = ''
 
-    // 1) Empty all space
-    document.getElementById('mainSpace').innerHTML = 'Done'
-
+    // Injection
+    document.getElementById('mainSpace').innerHTML = user.score
 }
 
 //* Start
