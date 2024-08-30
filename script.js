e94c9b1299a4307df3f5b08e8d405a482e2543ed
Mohammad Ashoori <mohammadashoori.87@gmail.com>
2024-08-30 01:58:59 -0700

    [Clean code js]

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let quizStatus = 0
  * This function indexing all quiz images in the array
  * @param {array} array An array to store all the quiz images
  */
-function setQuizImages(array){
+function setQuizImages(array) {
 
     // It covers all 30 folders: from 1 to 30
     for (let i = 0; i < 30; i++) {
@@ -25,14 +25,14 @@ function setQuizImages(array){
         )
 
         // from directory 1 - 12 that they are contain 6 images
-        if (i <= 11){
+        if (i <= 11) {
             for (let x = 1; x <= 6; x++) {
                 array[i].imageAnswers.push(
                     `${quizImagePath}/${testNumber}/${testNumber}-${x}.png`
                 )
             }
 
-        // from directory 13 - 30 that they are contain 8 images
+            // from directory 13 - 30 that they are contain 8 images
         } else {
             for (let x = 1; x <= 8; x++) {
                 array[i].imageAnswers.push(
@@ -48,7 +48,7 @@ function setQuizImages(array){
  * This function collects all quizzes id in a list
  * @param {array} array An array to store all the IDs
  */
-function setQuizListAllId(array){
+function setQuizListAllId(array) {
     for (let id = 0; id < quizImages.length; id++) {
         array.push(quizImages[id].id)
     }
@@ -59,7 +59,7 @@ function setQuizListAllId(array){
  * @param {array} array An array of objects: [{}, {}, {}, ...]
  * @returns {number} Random number between 0 to array.length
  */
-function getRandomQuiz(array){
+function getRandomQuiz(array) {
 
     // A) Receive quiz randomly
     let randomIndex = Math.floor(Math.random() * array.length)
@@ -68,7 +68,7 @@ function getRandomQuiz(array){
     // B) get images path
     let randomQuizPath = {}
     for (let i = 0; i < quizImages.length; i++) {
-        if (quizImages[i].id === randomQuiz){
+        if (quizImages[i].id === randomQuiz) {
             randomQuizPath = quizImages[i]
             break
         }
@@ -81,7 +81,7 @@ function getRandomQuiz(array){
 }
 
 /** This function implements the HTML structure of the welcome panel section */
-function injectTemplateWelcome(){
+function injectTemplateWelcome() {
     /*
     
     <div>
@@ -97,20 +97,20 @@ function injectTemplateWelcome(){
     let mainSpace = document.createElement('div')
     mainSpace.id = 'mainSpace'
     document.body.appendChild(mainSpace)
-    
+
     // <h1> name of project
     let appName = document.createElement('h1')
     appName.id = 'appName'
     appName.innerText = 'Superior IQ'
     mainSpace.appendChild(appName)
-    
+
     // <input> get name
     let userNameInput = document.createElement('input')
     userNameInput.className = 'textField'
     userNameInput.type = 'text'
     userNameInput.placeholder = 'Name'
     mainSpace.appendChild(userNameInput)
-    
+
     // <input> get age
     let userAgeInput = document.createElement('input')
     userAgeInput.className = 'textField'
@@ -125,16 +125,16 @@ function injectTemplateWelcome(){
     startBtn.value = 'Start'
     mainSpace.appendChild(startBtn)
     // if start button pressed: 
-    startBtn.addEventListener("click", function(){
+    startBtn.addEventListener("click", function () {
         nextQuiz()
     });
 }
 
 /** The general task of this function is to control the process of performing quiz */
-function nextQuiz(){
+function nextQuiz() {
 
     // 0) change quiz status
-    if (quizStatus < quizImages.length){
+    if (quizStatus < quizImages.length) {
         quizStatus++
     } else {
         return injectTemplateShowResult()
@@ -142,29 +142,29 @@ function nextQuiz(){
 
     // 1) Empty all space
     document.getElementById('mainSpace').innerHTML = ''
-    
+
     // 2) Get random image addresses of quiz
     let quizPaths = getRandomQuiz(quizListOfAllIds)
-    
+
     // 3) Implement HTML code
     console.log(quizPaths)
     injectTemplateQuiz(quizPaths)
-      
+
 }
 
 /**
  * This function implements the HTML part of the quiz
  * @param {object} path contain path of images
  */
-function injectTemplateQuiz(path){
-        /*
-    
-        <img>
-        <div>
-            <img> * (6 || 8)
-        </div>
+function injectTemplateQuiz(path) {
+    /*
+ 
+    <img>
+    <div>
+        <img> * (6 || 8)
+    </div>
 
-    */
+*/
 
     // <img> question image
     let questionImg = document.createElement('img')
@@ -180,7 +180,7 @@ function injectTemplateQuiz(path){
         let answerImg = document.createElement('img')
         answerImg.setAttribute('width', '70px')
         answerImg.setAttribute('src', path.imageAnswers[i])
-        answerImg.addEventListener("click", function(){
+        answerImg.addEventListener("click", function () {
             nextQuiz()
         });
         answerDiv.appendChild(answerImg)
@@ -191,7 +191,7 @@ function injectTemplateQuiz(path){
 /**
  * Implementation of the final panel that is supposed to show the result to the user
  */
-function injectTemplateShowResult(){
+function injectTemplateShowResult() {
 
     // 1) Empty all space
     document.getElementById('mainSpace').innerHTML = 'Done'
